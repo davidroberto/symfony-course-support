c4142d9f600344a0aa61f16a9e9dd394ca6c58cf
David Robert <contact.davidrobert@gmail.com>
2021-12-07 13:58:11 +0100

    entity manager

--- src/Controller/BookController.php
+++ src/Controller/BookController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Entity\Book;
 use App\Repository\BookRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -23,17 +24,27 @@ class BookController extends AbstractController
     /**
      * @Route("/book/create", name="book_create")
      */
-    public function createBook()
+    public function createBook(EntityManagerInterface $entityManager)
     {
+        // Je créé une nouvelle instance de la classe Book (de l'entité Book)
+        // dans le but de l'enregistrer en bdd
+        // Doctrine prendra l'entité avec les valeurs de chacune des propriétés
+        // et créera un enregistrement dans la table Book
         $book = new Book();
         $book->setTitle("Les thanatonautes");
         $book->setAuthor("Bernard Werber");
-        $book->setNbPages("700");
+        $book->setNbPages(700);
         $book->setPublishedAt(new \DateTime('1995-12-12'));
 
-        dump($book); die;
+        // une fois l'entité créée, j'utilise la classe EntityManager
+        // je demande à Symfony de l'instancier pour moi (grâce au système
+        // d'autowire)
+        // cette classe me permet de persister mon entité (de préparer sa sauvegarde
+        // en bdd), puis d'effectuer l'enregistrement (génère et éxecute une requête SQL)
+        $entityManager->persist($book);
+        $entityManager->flush();
 
-        // enregistrer l'instance de la classe Book (l'entité) en BDD
+        return $this->render('book_create.html.twig');
     }
 
     /**
