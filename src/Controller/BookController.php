6bfbcaca04ee9b46d47ab105c668ce31227deff2
David Robert <contact.davidrobert@gmail.com>
2021-12-07 11:09:58 +0100

    entity insert

--- src/Controller/BookController.php
+++ src/Controller/BookController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Book;
 use App\Repository\BookRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
@@ -14,16 +15,37 @@ class BookController extends AbstractController
     public function books(BookRepository $bookRepository)
     {
         $books = $bookRepository->findAll();
+
         return $this->render("books.html.twig",['books'=> $books]);
     }
 
+
     /**
-     * @Route ("/book/{id}", name="book")
+     * @Route("/book/create", name="book_create")
+     */
+    public function createBook()
+    {
+        $book = new Book();
+        $book->setTitle("Les thanatonautes");
+        $book->setAuthor("Bernard Werber");
+        $book->setNbPages("700");
+        $book->setPublishedAt(new \DateTime('1995-12-12'));
+
+        dump($book); die;
+
+        // enregistrer l'instance de la classe Book (l'entité) en BDD
+    }
+
+    /**
+     * @Route("/book/{id}", name="book")
      */
     public function book($id, BookRepository $bookRepository)
     {
         $book = $bookRepository->find($id);
+
         return $this->render("book.html.twig",['book'=> $book]);
     }
+
+
 
 }
